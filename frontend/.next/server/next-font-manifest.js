6a19b1770718eb30b6a4f63e1682aa7544410c5d
Noa-Miramont <noamiramont@gmail.com>
2025-05-11 15:13:02 +0200

    initialisation du projet nextjs

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/nm.mp4/Desktop/HETICVerse2/HETICVerse/frontend/src/app/layout\":[\"static/media/c6d08e7617bbcdce-s.p.woff2\",\"static/media/569ce4b8f30dc480-s.p.woff2\",\"static/media/93f479601ee12b01-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
